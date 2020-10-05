0df52bedaa747e32d6ab1ccfcc29f87db01e8be4
aDimitrov <nasko119@abv.bg>
2020-10-05 17:02:13 +0300

    local

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,15 +1,16 @@
-int arr[MAX_N], mn[MAX_N][LOG];
-int n;
-void construct() {
-    for(int i = 0; i < n; i ++) {mn[i][0] = arr[i];}
-    for(int j = 1; j < LOG; j ++) {
-        for(int i = 0; i + (1 << j) <= n; i ++) {
-            mn[i][j] = min(mn[i][j - 1], mn[i + (1 << (j - 1))][j - 1]);
+struct SparseTable {
+    int ans[LOG][MAX_N];
+    void construct(int n, int (&arr)[MAX_N]) {
+        for(int i = 0; i < n; i ++) {ans[0][i] = arr[i];}
+        for(int j = 1; j < LOG; j ++) {
+            for(int i = 0; i + (1 << j) <= n; i ++) {
+                ans[j][i] = min(ans[j - 1][i], ans[j - 1][i + (1 << (j - 1))]);
+            }
         }
     }
-}
-int query(int l, int r) {
-    int q = 0;
-    while((1 << q) <= r - l + 1) {q ++;} q --;
-    return min(mn[l][q], mn[r - (1 << q) + 1][q]);
-}
+    int query(int l, int r) {
+        int q = 0;
+        while((1 << q) <= r - l + 1) {q ++;} q --;
+        return min(ans[q][l], ans[q][r - (1 << q) + 1]);
+    }
+};
